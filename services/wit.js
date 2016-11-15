ef64f6a82da53f2c7ba17133ec603a959612934d
hossam-k <hossam.m.khalil@gmail.com>
2016-11-15 13:39:17 +0200

    remove delete.location

--- services/wit.js
+++ services/wit.js
@@ -45,7 +45,6 @@ var actions = {
 	merge(sessionId, context, entities, message, cb) {
 		// Reset the weather story
 		delete context.forecast
-		delete context.location
 		// Retrive the location entity and store it in the context field
 		var loc = firstEntityValue(entities, 'location')
 		if (loc) {
